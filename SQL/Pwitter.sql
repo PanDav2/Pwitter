5383a3a07fb08bba549d68fb97ac719132b93921
Tamazy <tamazy@laposte.net>
2014-02-18 02:28:04 +0100

    mydb changed to Cadene_Panou

--- SQL/Pwitter.sql
+++ SQL/Pwitter.sql
@@ -2,13 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `Cadene_Panou` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `Cadene_Panou` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Images`
+-- Table `Cadene_Panou`.`Images`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Images` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Images` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `url` VARCHAR(100) NOT NULL ,
   `tinyUrl` VARCHAR(45) NOT NULL ,
@@ -19,9 +19,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Users`
+-- Table `Cadene_Panou`.`Users`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Users` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
@@ -41,16 +41,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Users` (
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
   CONSTRAINT `fk_Users_table1`
     FOREIGN KEY (`image_id` )
-    REFERENCES `mydb`.`Images` (`id` )
+    REFERENCES `Cadene_Panou`.`Images` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pwitts`
+-- Table `Cadene_Panou`.`Pwitts`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Pwitts` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Pwitts` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `title` VARCHAR(45) NULL ,
   `content` VARCHAR(255) NOT NULL ,
@@ -60,16 +60,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Pwitts` (
   INDEX `fk_Pwitts_Users1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_Pwitts_Users1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Images_Pwitts`
+-- Table `Cadene_Panou`.`Images_Pwitts`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Images_Pwitts` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Images_Pwitts` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `image_id` INT NOT NULL ,
   `pwitt_id` INT NOT NULL ,
@@ -78,21 +78,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Images_Pwitts` (
   INDEX `fk_Images_Pwitts_Pwitts1_idx` (`pwitt_id` ASC) ,
   CONSTRAINT `fk_Images_Pwitts_Images1`
     FOREIGN KEY (`image_id` )
-    REFERENCES `mydb`.`Images` (`id` )
+    REFERENCES `Cadene_Panou`.`Images` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Images_Pwitts_Pwitts1`
     FOREIGN KEY (`pwitt_id` )
-    REFERENCES `mydb`.`Pwitts` (`id` )
+    REFERENCES `Cadene_Panou`.`Pwitts` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Hashtags`
+-- Table `Cadene_Panou`.`Hashtags`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Hashtags` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Hashtags` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `title` VARCHAR(45) NOT NULL ,
   `created` MEDIUMINT NOT NULL ,
@@ -102,16 +102,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Hashtags` (
   INDEX `fk_Hashtags_Users1_idx` (`created_by` ASC) ,
   CONSTRAINT `fk_Hashtags_Users1`
     FOREIGN KEY (`created_by` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Hashtages_Pwitts`
+-- Table `Cadene_Panou`.`Hashtages_Pwitts`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Hashtages_Pwitts` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Hashtages_Pwitts` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `hashtag_id` INT NOT NULL ,
   `pwitt_id` INT NOT NULL ,
@@ -120,21 +120,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Hashtages_Pwitts` (
   INDEX `fk_Hashtages_Pwitts_Hashtags1_idx` (`hashtag_id` ASC) ,
   CONSTRAINT `fk_Hashtages_Pwitts_Pwitts1`
     FOREIGN KEY (`pwitt_id` )
-    REFERENCES `mydb`.`Pwitts` (`id` )
+    REFERENCES `Cadene_Panou`.`Pwitts` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Hashtages_Pwitts_Hashtags1`
     FOREIGN KEY (`hashtag_id` )
-    REFERENCES `mydb`.`Hashtags` (`id` )
+    REFERENCES `Cadene_Panou`.`Hashtags` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Followers`
+-- Table `Cadene_Panou`.`Followers`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Followers` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Followers` (
   `id` INT NOT NULL ,
   `user_id` INT NOT NULL ,
   `follower_id` INT NOT NULL ,
@@ -144,21 +144,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Followers` (
   INDEX `fk_Followers_Users2_idx` (`follower_id` ASC) ,
   CONSTRAINT `fk_Followers_Users1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Followers_Users2`
     FOREIGN KEY (`follower_id` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Albums`
+-- Table `Cadene_Panou`.`Albums`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Albums` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Albums` (
   `id` INT NOT NULL ,
   `title` VARCHAR(45) NOT NULL ,
   `desc` TEXT NOT NULL ,
@@ -167,16 +167,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Albums` (
   INDEX `fk_Albums_Users1_idx` (`Users_id` ASC) ,
   CONSTRAINT `fk_Albums_Users1`
     FOREIGN KEY (`Users_id` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Albums_Images`
+-- Table `Cadene_Panou`.`Albums_Images`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Albums_Images` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Albums_Images` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `album_id` INT NOT NULL ,
   `image_id` INT NOT NULL ,
@@ -185,21 +185,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Albums_Images` (
   INDEX `fk_Albums_Images_Images1_idx` (`image_id` ASC) ,
   CONSTRAINT `fk_Albums_Images_Albums1`
     FOREIGN KEY (`album_id` )
-    REFERENCES `mydb`.`Albums` (`id` )
+    REFERENCES `Cadene_Panou`.`Albums` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Albums_Images_Images1`
     FOREIGN KEY (`image_id` )
-    REFERENCES `mydb`.`Images` (`id` )
+    REFERENCES `Cadene_Panou`.`Images` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Notifs`
+-- Table `Cadene_Panou`.`Notifs`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Notifs` (
+CREATE  TABLE IF NOT EXISTS `Cadene_Panou`.`Notifs` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `content` VARCHAR(255) NOT NULL ,
   `isViewed` TINYINT NOT NULL ,
@@ -210,12 +210,12 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Notifs` (
   INDEX `fk_Notifs_Users1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_Notifs_Users1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`Users` (`id` )
+    REFERENCES `Cadene_Panou`.`Users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `mydb` ;
+USE `Cadene_Panou` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
